ba23c6b4a32146124d54a730926e4b1900ada9a6
ziirish <ziirish@ziirish.info>
2014-08-25 10:07:32 +0200

    add: setup.py

---
+++ setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import sys
+import re
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.version < '3':
+    import codecs
+    def u(x):
+        return codecs.unicode_escape_decode(x)[0]
+else:
+    def u(x):
+        return x
+
+def get_version():
+    VERSIONFILE = 'burpui/__init__.py'
+    initfile_lines = open(VERSIONFILE, 'rt').readlines()
+    VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    for line in initfile_lines:
+        mo = re.search(VSRE, line, re.M)
+        if mo:
+            return u(mo.group(1))
+    raise RuntimeError('Unable to find version string in %s.' % (VERSIONFILE,))
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+setup(
+    name='burp-ui',
+    version=get_version(),
+    description=u('Burp-UI is a web-ui for burp backup written in python with Flask and jQuery/Bootstrap'),
+    long_description=open('README.md').read(),
+    license=open('LICENSE').read(),
+    author=u('Benjamin SANS (Ziirish)'),
+    author_email=u('ziirish@ziirish.info'),
+    url='https://github.com/ziirish/burp-ui',
+    keywords='burp web ui',
+    packages=['burpui'],
+    install_requires=['Flask==0.10.1', 'Flask-Login==0.2.11', 'Flask-WTF==0.10.0', 'WTForms==2.0.1'],
+    extras_require = {
+        'ldap_authentication': ['simpleldap==0.8']
+    },
+    classifiers=[
+        'Framework :: Flask',
+        'Intended Audience :: System Administrators',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: System :: Archiving :: Backup',
+        'Topic :: System :: Monitoring'
+    ]
+)
