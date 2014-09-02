7159cfedc8247df39b1caf9847d28bf5b8b15cbc
ziirish <ziirish@ziirish.info>
2014-09-02 08:44:29 +0200

    add: remember me feature

--- burpui/forms.py
+++ burpui/forms.py
@@ -1,9 +1,10 @@
 # -*- coding: utf8 -*-
 
 from flask_wtf import Form
-from wtforms import TextField, PasswordField, validators
+from wtforms import TextField, PasswordField, BooleanField, validators
 
 class LoginForm(Form):
     username = TextField('Username', [validators.Length(min=2, max=25)])
     password = PasswordField('Password', [validators.Required()])
+    remember = BooleanField('Remember me', [validators.Optional()])
 
