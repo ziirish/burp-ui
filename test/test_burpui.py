66ba97bab1ad23cb5f9136c2374c060e9430d757
ziirish <ziirish@ziirish.info>
2014-11-26 16:49:28 +0100

    add authentication unit test

--- test/test_burpui.py
+++ test/test_burpui.py
@@ -9,7 +9,7 @@ from flask.ext.testing import LiveServerTestCase, TestCase
 
 sys.path.append('{0}/..'.format(os.path.join(os.path.dirname(os.path.realpath(__file__)))))
 
-from burpui import app, bui, login_manager
+from burpui import app, bui, login_manager, init as BUIinit
 
 class BurpuiLiveTestCase(LiveServerTestCase):
 
@@ -52,10 +52,46 @@ class BurpuiTestCase(TestCase):
 		login_manager.init_app(app)
 		return app
 
-	def test_some_json(self):
+	def test_no_clients(self):
 		response = self.client.get('/api/clients.json')
 		self.assertNotEquals(response.json, dict(results=[]))
 
+class BurpuiLoginTestCase(TestCase):
+
+	def setUp(self):
+		print '\nBegin Test 3\n'
+
+	def tearDown(self):
+		print '\nTest 3 Finished!\n'
+
+	def login(self, username, password):
+		return self.client.post('/login', data=dict(
+			username=username,
+			password=password
+		), follow_redirects=True)
+
+	def create_app(self):
+		conf = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../burpui.cfg')
+		BUIinit(conf, False, False)
+		app.config['TESTING'] = True
+		app.config['LIVESERVER_PORT'] = 5001
+		app.config['WTF_CSRF_ENABLED'] = False
+		bui.cli.port = 9999
+		login_manager.init_app(app)
+		return app
+
+	def test_login_ok(self):
+		rv = self.login('admin', 'admin')
+		print rv.data
+		assert 'Logged in successfully' in rv.data
+	
+	def test_login_ko(self):
+		rv = self.login('admin', 'toto')
+		assert 'Wrong username or password' in rv.data
+
+	def test_login_no_user(self):
+		rv = self.login('toto', 'toto')
+		assert 'Wrong username or password' in rv.data
 
 if __name__ == '__main__':
 	unittest.main()
