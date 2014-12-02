d4e074e3ddbdb1bb6740343ec058bf95297ce33d
ziirish <ziirish@ziirish.info>
2014-12-02 11:04:56 +0100

    improve test suite script

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
-echo "install lib devel..."
-apt-get update
-apt-get -y install python-pip python
-#apt-get -y install python2.7-dev python2.6-dev libsasl2-dev
+PIP=$(which pip)
+PYTHON=$(which python2.7)
+ISROOT=0
+
+[ $UID -eq 0 ] && ISROOT=1
+
+echo "test requirements"
+[ $ISROOT -eq 1 ] && apt-get update
+
+[ -x "$PIP" ] && {
+    echo "python-pip seems to be installed"
+} || {
+    echo "python-pip is missing... Installing it"
+    [ $ISROOT -eq 1 ] && apt-get -y install python-pip
+}
+
+[ -x "$PYTHON" ] && {
+    echo "python2.7 seems to be installed"
+} || {
+    echo "python2.7 is missing... Installing it"
+    [ $ISROOT -eq 1 ] && apt-get -y install python2.7 python
+}
+#echo "install lib devel..."
+#apt-get update
+#apt-get -y install python-pip python
+##apt-get -y install python2.7-dev python2.6-dev libsasl2-dev
 
 echo "check files"
 pwd
@@ -13,21 +35,20 @@ file burpui.cfg
 file test/test_burpui.py
 
 echo "install virtualenv"
-pip install virtualenv
+$PIP install virtualenv
 mkdir py2.7
 
 echo "test python2.7"
-virtualenv -p /usr/bin/python2.7 py2.7
+virtualenv -p $PYTHON py2.7
 source py2.7/bin/activate
 pip install -r requirements.txt
 pip install -r test-requirements.txt
 LOG=$(mktemp)
 (
-nosetests --with-coverage --cover-package=burpui test/test_burpui.py
-ret=$?
-exit $ret
+    nosetests --with-coverage --cover-package=burpui test/test_burpui.py
+    exit $?
 ) &>$LOG
-ret2=$?
+ret=$?
 cat $LOG
 grep TOTAL $LOG | awk '{ print "TOTAL: "$4; }'
 rm $LOG
@@ -35,6 +56,6 @@ deactivate
 
 echo "That's it!"
 
-echo "Return: $ret2"
+echo "Return: $ret"
 
-exit $ret2
+exit $ret
