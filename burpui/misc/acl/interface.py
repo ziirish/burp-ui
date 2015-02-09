f42600de678c35cc2380b42ce42132663cd9830b
ziirish <ziirish@ziirish.info>
2015-02-09 18:08:20 +0100

    add: start implementing ACL support for #47

---
+++ burpui/misc/acl/interface.py
@@ -0,0 +1,11 @@
+# -*- coding: utf8 -*-
+
+class BUIaclLoader:
+    def get_acl(self):
+        raise NotImplementedError("Sorry, the current Backend does not implement this method!")
+
+class BUIacl:
+    def is_admin(self, username=None):
+        raise NotImplementedError("Sorry, the current Backend does not implement this method!")
+    def clients(self, username=None):
+        raise NotImplementedError("Sorry, the current Backend does not implement this method!")
