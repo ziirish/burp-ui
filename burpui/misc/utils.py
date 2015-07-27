754188d858bb785e4a5e48c8330c94adecf9bcac
ziirish <ziirish@ziirish.info>
2015-07-27 19:48:26 +0200

    pep8

--- burpui/misc/utils.py
+++ burpui/misc/utils.py
@@ -7,53 +7,58 @@ It was found on the Internet...
 
 import math
 import string
+import sys
+import inspect
+import zipfile
+import tarfile
 
-# code from here: http://code.activestate.com/recipes/578323-human-readable-filememory-sizes-v2/
-class human_readable( long ):
+
+class human_readable(long):
     """ define a human_readable class to allow custom formatting
-        format specifiers supported : 
-            em : formats the size as bits in IEC format i.e. 1024 bits (128 bytes) = 1Kib 
+        format specifiers supported :
+            em : formats the size as bits in IEC format i.e. 1024 bits (128 bytes) = 1Kib
             eM : formats the size as Bytes in IEC format i.e. 1024 bytes = 1KiB
             sm : formats the size as bits in SI format i.e. 1000 bits = 1kb
             sM : formats the size as bytes in SI format i.e. 1000 bytes = 1KB
             cm : format the size as bit in the common format i.e. 1024 bits (128 bytes) = 1Kb
             cM : format the size as bytes in the common format i.e. 1024 bytes = 1KB
+
+        code from: http://code.activestate.com/recipes/578323-human-readable-filememory-sizes-v2/
     """
     def __format__(self, fmt):
         # is it an empty format or not a special format for the size class
-        if fmt == "" or fmt[-2:].lower() not in ["em","sm","cm"]:
-            if fmt[-1].lower() in ['b','c','d','o','x','n','e','f','g','%']:
+        if fmt == "" or fmt[-2:].lower() not in ["em", "sm", "cm"]:
+            if fmt[-1].lower() in ['b', 'c', 'd', 'o', 'x', 'n', 'e', 'f', 'g', '%']:
                 # Numeric format.
                 return long(self).__format__(fmt)
             else:
                 return str(self).__format__(fmt)
 
-        # work out the scale, suffix and base        
-        factor, suffix = (8, "b") if fmt[-1] in string.lowercase else (1,"B")
-        base = 1024 if fmt[-2] in ["e","c"] else 1000
+        # work out the scale, suffix and base
+        factor, suffix = (8, "b") if fmt[-1] in string.lowercase else (1, "B")
+        base = 1024 if fmt[-2] in ["e", "c"] else 1000
 
         # Add the i for the IEC format
-        suffix = "i"+ suffix if fmt[-2] == "e" else suffix
+        suffix = "i" + suffix if fmt[-2] == "e" else suffix
 
-        mult = ["","K","M","G","T","P"]
+        mult = ["", "K", "M", "G", "T", "P"]
 
         val = float(self) * factor
-        i = 0 if val < 1 else int(math.log(val, base))+1
-        v = val / math.pow(base,i)
-        v,i = (v,i) if v > 0.5 else (v*base,i-1)
+        i = 0 if val < 1 else int(math.log(val, base)) + 1
+        v = val / math.pow(base, i)
+        v, i = (v, i) if v > 0.5 else (v * base, i - 1)
 
         # Identify if there is a width and extract it
-        width = "" if fmt.find(".") == -1 else fmt[:fmt.index(".")]        
+        width = "" if fmt.find(".") == -1 else fmt[:fmt.index(".")]
         precis = fmt[:-2] if width == "" else fmt[fmt.index("."):-2]
 
         # do the precision bit first, so width/alignment works with the suffix
         if float(self) == 0:
             return "{0:{1}f}".format(v, precis)
-        t = ("{0:{1}f}"+mult[i]+suffix).format(v, precis) 
+        t = ("{0:{1}f}" + mult[i] + suffix).format(v, precis)
 
-        return "{0:{1}}".format(t,width) if width != "" else t
+        return "{0:{1}}".format(t, width) if width != "" else t
 
-import sys
 
 def currentframe():
     """Return the frame object for the caller's stack frame."""
@@ -62,7 +67,6 @@ def currentframe():
     except:
         return sys.exc_info()[2].tb_frame.f_back
 
-import inspect
 
 class BUIlogging(object):
     def _logger(self, level, *args):
@@ -86,14 +90,12 @@ class BUIlogging(object):
                     """
                     ar = list(args)
                     if isinstance(ar[0], str):
-                        ar[0] = filename+':'+str(cf.f_lineno)+' => '+ar[0]
+                        ar[0] = filename + ':' + str(cf.f_lineno) + ' => ' + ar[0]
                     else:
-                        ar = [filename+':'+str(cf.f_lineno)+' => {0}'.format(ar)]
+                        ar = [filename + ':' + str(cf.f_lineno) + ' => {0}'.format(ar)]
                     args = tuple(ar)
                 logs[level](*args)
 
-import zipfile
-import tarfile
 
 class BUIcompress():
     def __init__(self, name, archive):
