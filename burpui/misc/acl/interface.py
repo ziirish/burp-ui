757dfc0e33f8cb21d79906207ba450bfdcaeb19e
ziirish <ziirish@ziirish.info>
2015-07-27 14:56:35 +0200

    pep8

--- burpui/misc/acl/interface.py
+++ burpui/misc/acl/interface.py
@@ -1,16 +1,21 @@
 # -*- coding: utf8 -*-
 
+
 class BUIaclLoader:
     @property
     def acl(self):
         raise NotImplementedError("Sorry, the current Backend does not implement this method!")
 
+
 class BUIacl:
     def is_admin(self, username=None):
         raise NotImplementedError("Sorry, the current Backend does not implement this method!")
+
     def clients(self, username=None, server=None):
         raise NotImplementedError("Sorry, the current Backend does not implement this method!")
+
     def servers(self, username=None):
         raise NotImplementedError("Sorry, the current Backend does not implement this method!")
+
     def is_client_allowed(self, username=None, client=None, server=None):
         raise NotImplementedError("Sorry, the current Backend does not implement this method!")
