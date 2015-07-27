e5ad9be153260e22c84caf076ad55d44f5cbf3b0
ziirish <ziirish@ziirish.info>
2015-07-27 19:23:28 +0200

    pep8

--- burpui/misc/auth/interface.py
+++ burpui/misc/auth/interface.py
@@ -1,11 +1,13 @@
 # -*- coding: utf8 -*-
 
+
 class BUIhandler:
     def __init__(self, app=None):
         pass
 
     def user(self, name=None):
         raise NotImplementedError("Sorry, the current Backend does not implement this method!")
+
 
 class BUIuser:
     def login(self, name=None, passwd=None):
