76c652fb875842986962cf9fa0337cb0d5e60abd
ziirish <ziirish@ziirish.info>
2015-09-30 10:17:28 +0200

    fix: python3 compatibility

--- burpui/misc/utils.py
+++ burpui/misc/utils.py
@@ -70,17 +70,30 @@ def currentframe():
     except:
         return sys.exc_info()[2].tb_frame.f_back
 
-class BUIlogger(logging.Logger):
-    def makeRecord(self, name, level, fn, lno, msg, args, exc_info, func=None, extra=None):
-        """
-        Try to guess where was call the function
-        """
-        cf = currentframe()
-        (frame, filename, line_number, function_name, lines, index) = inspect.getouterframes(cf)[4]
-        if cf is not None:
-            cf = cf.f_back
-            return super(BUIlogger, self).makeRecord(name, level, filename, line_number, msg, args, exc_info, func=function_name, extra=extra)
-        return super(BUIlogger, self).makeRecord(name, level, fn, lno, msg, args, exc_info, func=func, extra=extra)
+if sys.version_info >= (3, 0):
+    class BUIlogger(logging.Logger):
+        def makeRecord(self, name, level, fn, lno, msg, args, exc_info, func=None, extra=None, sinfo=None):
+            """
+            Try to guess where was call the function
+            """
+            cf = currentframe()
+            (frame, filename, line_number, function_name, lines, index) = inspect.getouterframes(cf)[4]
+            if cf is not None:
+                cf = cf.f_back
+                return super(BUIlogger, self).makeRecord(name, level, filename, line_number, msg, args, exc_info, func=function_name, extra=extra, sinfo=sinfo)
+            return super(BUIlogger, self).makeRecord(name, level, fn, lno, msg, args, exc_info, func=func, extra=extra, sinfo=sinfo)
+else:
+    class BUIlogger(logging.Logger):
+        def makeRecord(self, name, level, fn, lno, msg, args, exc_info, func=None, extra=None):
+            """
+            Try to guess where was call the function
+            """
+            cf = currentframe()
+            (frame, filename, line_number, function_name, lines, index) = inspect.getouterframes(cf)[4]
+            if cf is not None:
+                cf = cf.f_back
+                return super(BUIlogger, self).makeRecord(name, level, filename, line_number, msg, args, exc_info, func=function_name, extra=extra)
+            return super(BUIlogger, self).makeRecord(name, level, fn, lno, msg, args, exc_info, func=func, extra=extra)
 
 
 class BUIlogging(object):
