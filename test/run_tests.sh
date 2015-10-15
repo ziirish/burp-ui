e692ba849528bc3a9be2784668509089cb266a2f
ziirish <ziirish@ziirish.info>
2015-10-15 22:35:34 +0200

    fix tests

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -38,7 +38,7 @@ echo "test requirements"
 
 echo "install build requirements"
 update
-[ $ISROOT -eq 1 ] && apt-get install -y uthash-dev g++ make libssl-dev librsync-dev nodejs npm python$(perl -pe "s/\.\d+$//" <<<$VERSION)-dev
+[ $ISROOT -eq 1 ] && apt-get install -y uthash-dev g++ make libssl-dev librsync-dev nodejs nodejs-legacy npm python$(perl -pe "s/\.\d+$//" <<<$VERSION)-dev
 
 echo "installing bower"
 npm install -g bower
