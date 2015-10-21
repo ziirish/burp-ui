63332873e53abcce8632ffd2547f7099cecb3ab4
ziirish <ziirish@ziirish.info>
2015-10-21 11:23:24 +0200

    add: build development package

---
+++ test/run_build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+PYTHON=$(which python)
+ISROOT=0
+UPDATED=0
+
+function update() {
+    [ $UPDATED -eq 0 ] && [ $ISROOT -eq 1 ] && {
+        apt-get update
+        UPDATED=1
+    }
+    return 0
+}
+
+echo "test requirements"
+[ $UID -eq 0 ] && ISROOT=1
+
+[ -x "$PYTHON" ] && {
+    echo "python seems to be installed"
+} || {
+    echo "python is missing..."
+    exit 1
+}
+
+echo "install build requirements"
+update
+[ $ISROOT -eq 1 ] && apt-get install -y nodejs nodejs-legacy npm
+
+echo "installing bower"
+npm install -g bower
+
+echo "building dist"
+$PYTHON setup.py sdist
+
+echo "publishing build"
+cp dist/burp-ui*.tar.gz /builds/burp-ui.dev.tar.gz
