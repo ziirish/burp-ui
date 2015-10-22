a08725fa85164110997fe78f089de247c4b1c2fb
ziirish <ziirish@ziirish.info>
2015-10-22 20:52:32 +0200

    new burp2 options

--- burpui/misc/parser/burp2.py
+++ burpui/misc/parser/burp2.py
@@ -6,3 +6,17 @@ from .burp1 import Parser as Burp1
 class Parser(Burp1):
     """Implements :class:`burpui.misc.parser.burp1.Parser`"""
     pver = 2
+
+    boolean_srv = Burp1.boolean_srv + [
+        u'acl',
+        u'xattr',
+    ]
+    fields_cli = Burp1.fields_cli + [
+        u'acl',
+        u'xattr',
+    ]
+    placeholders = Burp1.placeholders
+    placeholders.update({
+        u'acl': u"0|1",
+        u'xattr': u"0|1",
+    })
