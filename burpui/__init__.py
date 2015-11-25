10e8e8adf5fb8c148cb07df21fdbc58cdf6af8db
ziirish <ziirish@ziirish.info>
2015-11-25 23:00:30 +0100

    add: verbose version

--- burpui/__init__.py
+++ burpui/__init__.py
@@ -14,13 +14,6 @@ import os
 import sys
 import logging
 
-from flask import Flask
-from flask.ext.login import LoginManager, login_user
-from flask.ext.bower import Bower
-from .server import BUIServer as BurpUI
-from .routes import view
-from .api import api, apibp
-
 if sys.version_info < (3, 0):
     reload(sys)
     sys.setdefaultencoding('utf-8')
@@ -32,68 +25,12 @@ __url__ = 'https://git.ziirish.me/ziirish/burp-ui'
 __description__ = 'Burp-UI is a web-ui for burp backup written in python with Flask and jQuery/Bootstrap'
 __license__ = 'BSD 3-clause'
 __version__ = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'VERSION')).read().rstrip()
+try:
+    __release__ = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'RELEASE')).read().rstrip()
+except:
+    __release__ = 'unknown'
 
-
-# First, we setup the app
-app = Flask(__name__)
-
-app.config['CFG'] = None
-
-app.secret_key = 'VpgOXNXAgcO81xFPyWj07ppN6kExNZeCDRShseNzFKV7ZCgmW2/eLn6xSlt7pYAVBj12zx2Vv9Kw3Q3jd1266A=='
-app.jinja_env.globals.update(isinstance=isinstance, list=list)
-
-# We initialize the core
-bui = BurpUI(app)
-
-# Then we load our routes
-view.init_bui(bui)
-app.register_blueprint(view)
-
-# We initialize the API
-api.init_bui(bui)
-api.version = __version__
-app.register_blueprint(apibp)
-
-
-# And the login_manager
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'view.login'
-login_manager.login_message_category = 'info'
-
-app.config.setdefault('BOWER_COMPONENTS_ROOT', os.path.join('static', 'vendor'))
-app.config.setdefault('BOWER_REPLACE_URL_FOR', True)
-bower = Bower()
-bower.init_app(app)
-
-
-@login_manager.user_loader
-def load_user(userid):
-    """User loader callback"""
-    if bui.auth != 'none':
-        return bui.uhandler.user(userid)
-    return None  # pragma: no cover
-
-
-@login_manager.request_loader
-def load_user_from_request(request):
-    """User loader from request callback"""
-    if bui.auth != 'none':
-        creds = request.headers.get('Authorization')
-        if creds:
-            creds = creds.replace('Basic ', '', 1)
-            try:
-                import base64
-                login, password = base64.b64decode(creds).split(':')
-            except:
-                pass
-            if login:
-                user = bui.uhandler.user(login)
-                if user.active and user.login(login, password):
-                    login_user(user)
-                    return user
-
-    return None
+bui = None
 
 
 def lookup_config(conf=None):
@@ -101,7 +38,6 @@ def lookup_config(conf=None):
     if conf:
         if os.path.isfile(conf):
             ret = conf
-            app.config['CFG'] = conf
         else:
             raise IOError('File not found: \'{0}\''.format(conf))
     else:
@@ -117,13 +53,10 @@ def lookup_config(conf=None):
             os.path.join(root, 'burpui.sample.cfg')
         ]
         for p in conf_files:
-            app.logger.debug('Trying file \'{}\''.format(p))
             if os.path.isfile(p):
-                app.config['CFG'] = p
                 ret = p
                 break
 
-    app.logger.debug('Using file \'{}\''.format(ret))
     return ret
 
 
@@ -144,6 +77,68 @@ def init(conf=None, debug=0, logfile=None, gunicorn=True):
 
     :returns: A :class:`Flask` object
     """
+    from flask.ext.login import LoginManager, login_user
+    from flask.ext.bower import Bower
+    from .server import BUIServer as BurpUI
+    from .routes import view
+    from .api import api, apibp
+
+    # We initialize the core
+    bui = BurpUI()
+
+    bui.config['CFG'] = None
+
+    bui.secret_key = 'VpgOXNXAgcO81xFPyWj07ppN6kExNZeCDRShseNzFKV7ZCgmW2/eLn6xSlt7pYAVBj12zx2Vv9Kw3Q3jd1266A=='
+    bui.jinja_env.globals.update(isinstance=isinstance, list=list)
+
+    # Then we load our routes
+    view.init_bui(bui)
+    bui.register_blueprint(view)
+
+    # We initialize the API
+    api.init_bui(bui)
+    api.version = __version__
+    api.release = __release__
+    bui.register_blueprint(apibp)
+
+    # And the login_manager
+    login_manager = LoginManager()
+    login_manager.init_app(bui)
+    login_manager.login_view = 'view.login'
+    login_manager.login_message_category = 'info'
+
+    bui.config.setdefault('BOWER_COMPONENTS_ROOT', os.path.join('static', 'vendor'))
+    bui.config.setdefault('BOWER_REPLACE_URL_FOR', True)
+    bower = Bower()
+    bower.init_app(bui)
+
+    @login_manager.user_loader
+    def load_user(userid):
+        """User loader callback"""
+        if bui.auth != 'none':
+            return bui.uhandler.user(userid)
+        return None  # pragma: no cover
+
+    @login_manager.request_loader
+    def load_user_from_request(request):
+        """User loader from request callback"""
+        if bui.auth != 'none':
+            creds = request.headers.get('Authorization')
+            if creds:
+                creds = creds.replace('Basic ', '', 1)
+                try:
+                    import base64
+                    login, password = base64.b64decode(creds).split(':')
+                except:
+                    pass
+                if login:
+                    user = bui.uhandler.user(login)
+                    if user.active and user.login(login, password):
+                        login_user(user)
+                        return user
+
+        return None
+
     # The debug argument used to be a boolean so we keep supporting this format
     if isinstance(debug, bool):
         if debug:
@@ -159,8 +154,8 @@ def init(conf=None, debug=0, logfile=None, gunicorn=True):
         debug = levels[debug]
 
     if debug != logging.NOTSET and not gunicorn:  # pragma: no cover
-        app.config['DEBUG'] = True
-        app.config['TESTING'] = True
+        bui.config['DEBUG'] = True
+        bui.config['TESTING'] = True
 
     if logfile:
         from logging import Formatter
@@ -177,16 +172,16 @@ def init(conf=None, debug=0, logfile=None, gunicorn=True):
             LOG_FORMAT = '[%(asctime)s] %(levelname)s in %(module)s.%(funcName)s: %(message)s'
         file_handler.setLevel(debug)
         file_handler.setFormatter(Formatter(LOG_FORMAT))
-        app.logger.addHandler(file_handler)
+        bui.logger.addHandler(file_handler)
 
     # Still need to test conf file here because the init function can be called
     # by gunicorn directly
-    lookup_config(conf)
+    bui.config['CFG'] = lookup_config(conf)
 
-    bui.setup(app.config['CFG'])
+    bui.setup(bui.config['CFG'])
 
     if gunicorn:  # pragma: no cover
         from werkzeug.contrib.fixers import ProxyFix
-        app.wsgi_app = ProxyFix(app.wsgi_app)
+        bui.wsgi_app = ProxyFix(bui.wsgi_app)
 
-    return app
+    return bui
