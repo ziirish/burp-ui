ff906e9419bcccc3d1217d90a416ad70c22bd909
ziirish <ziirish@ziirish.info>
2016-06-08 18:56:38 +0200

    add: CA.cnf parser

---
+++ burpui/misc/parser/openssl.py
@@ -0,0 +1,136 @@
+# -*- coding: utf8 -*-
+"""
+.. module:: burpui.misc.parser.openssl
+    :platform: Unix
+    :synopsis: Burp-UI configuration file parser OpenSSL configuration.
+.. moduleauthor:: Ziirish <hi+burpui@ziirish.me>
+"""
+import os
+import re
+import codecs
+
+from hashlib import md5
+from six import iteritems
+
+
+class OSSLConf(object):
+    """Parse the given OpenSSL configuration file
+
+    :param conffile: Configuration file to parse
+    :type conffile: str
+    """
+
+    def __init__(self, conffile=None):
+        self.conffile = conffile
+        self.conf = {}
+        self.env_cache = {}
+        self.md5 = None
+
+    @property
+    def values(self):
+        """Mapping of the configuration file into a dict"""
+        self._parse()
+        return self.conf
+
+    def _read(self):
+        """Read the file if possible"""
+        ret = []
+        try:
+            with codecs.open(self.conffile, 'r', 'utf-8') as fil:
+                ret = [x.rstrip('\n') for x in fil.readlines()]
+        except IOError:
+            pass
+        return ret
+
+    def _parse(self):
+        """Parse the file if it has changed since last read and replace all
+        variables using a multi-passes loop
+        """
+        dic = {}
+        chksum = self._md5(self.conffile)
+        if self.md5 and self.md5 == chksum:
+            return
+
+        for line in self._read():
+            if re.match(r'^\s*#', line):
+                continue
+            res = re.search(r'\s*([^#][^=\s]+)\s*=\s*(.*)$', line)
+            if res:
+                key = res.group(1)
+                val = res.group(2)
+
+                dic[key] = val
+
+        for key, val in iteritems(dic):
+            self.conf[key] = dic[key] = val = self._translate(dic, key, val)
+
+        self.md5 = chksum
+
+    def _translate(self, temp, key, val):
+        """Translate variables if needed until the returned value does not seem
+        to contain variables anymore
+        """
+        res = val
+        dic = temp
+        env = self._is_env(res)
+        if env:
+            for match in env:
+                res = dic[key] = self._translate_env(
+                    dic,
+                    match,
+                    res
+                )
+        lcl = self._is_local(res)
+        if lcl:
+            for match in lcl:
+                res = dic[key] = self._translate_local(
+                    dic,
+                    match,
+                    res
+                )
+        if not lcl and not env:
+            return res
+        return self._translate(dic, key, res)
+
+    @staticmethod
+    def _is_local(val):
+        """Look for 'local' variables (ie. $dir)"""
+        lcl = re.compile(r'\${?(\w+)}?')
+        res = [x for x in lcl.findall(val) if x.lower() != 'env']
+        return res if res else False
+
+    def _translate_local(self, temp, pattern, val):
+        """Translate 'local' variables (ie. $dir is replaced by the content of
+        the 'dir' variable)
+        """
+        res = self.conf.get(pattern) or temp.get(pattern) or ''
+        return re.sub(r'\${?' + pattern + '}?', res, val)
+
+    @staticmethod
+    def _is_env(val):
+        """Look for 'global' variables (ie. $ENV::HOME)"""
+        env = re.compile(r'\${?ENV::(\w+)}?')
+        res = env.findall(val)
+        return res if res else False
+
+    def _translate_env(self, temp, pattern, val):
+        """Translate 'global' variables (ie. $ENV::HOME is replace by the
+        content of the 'HOME' variable if available in the file or with the
+        $HOME environment variable
+        """
+        res = self.env_cache.get(pattern) or os.getenv(pattern) or \
+            self.conf.get(pattern) or temp.get(pattern) or ''
+        self.env_cache[pattern] = res
+        return re.sub(r'\${?ENV::' + pattern + '}?', res, val)
+
+    @staticmethod
+    def _md5(path):
+        """Compute the md5sum of the configuration file to detect changes"""
+        hash_md5 = md5()
+        try:
+            with open(path, "rb") as bfile:
+                for chunk in iter(lambda: bfile.read(4096), b""):
+                    hash_md5.update(chunk)
+            return hash_md5.hexdigest()
+        except IOError:
+            return None
