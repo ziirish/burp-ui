31c3fea11dd502952373dc58799b9a1d264c02b1
ziirish <ziirish@ziirish.info>
2016-07-20 22:50:33 +0200

    fix: doc

--- burpui/manage.py
+++ burpui/manage.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+Burp-UI is a web-ui for burp backup written in python with Flask and
+jQuery/Bootstrap
+
+.. module:: burpui.manage
+    :platform: Unix
+    :synopsis: Burp-UI Manager app.
+
+.. moduleauthor:: Ziirish <hi+burpui@ziirish.me>
+"""
 import os
 import sys
 
