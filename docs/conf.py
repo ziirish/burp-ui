207b4fe3d10095d3dd7ba00f088e624952e3c9be
ziirish <ziirish@ziirish.info>
2017-01-04 13:24:37 +0100

    prepare next release

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Burp-UI'
-copyright = u'2016, Ziirish'
+copyright = u'2017, Ziirish'
 author = u'Ziirish'
 
 # The version info for the project you're documenting, acts as replacement for
