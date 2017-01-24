220aa19749af38ebc1319c28649ef0a3f76d6bb1
ziirish <ziirish@ziirish.info>
2017-01-24 22:07:16 +0100

    fix base exception type

--- burpui/exceptions.py
+++ burpui/exceptions.py
@@ -11,7 +11,7 @@
 try:
     from werkzeug.exceptions import HTTPException
 except ImportError:
-    HTTPException = object
+    HTTPException = Exception
 
 
 class BUIserverException(HTTPException):
