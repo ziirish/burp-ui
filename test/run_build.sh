a37c680530a048ea308fb7819d5be4319a98fe58
ziirish <ziirish@ziirish.info>
2017-01-26 22:15:56 +0100

    build meta-packages too

--- test/run_build.sh
+++ test/run_build.sh
@@ -17,23 +17,43 @@ pip install -r requirements.txt
 pip install wheel
 
 echo "building dist"
-[ "$(sed 's/\([[:digit:]]*\)\..*$/\1/' <<<$VERSION)" -eq 2 ] && $PYTHON setup.py sdist bdist_wheel
-$PYTHON setup.py bdist_egg
+[ "$(sed 's/\([[:digit:]]*\)\..*$/\1/' <<<$VERSION)" -eq 2 ] && {
+    $PYTHON setup.py sdist bdist_wheel bdist_egg
+    cd pkgs
+    for pkg in *
+    do
+        cd $pkg
+        $PYTHON setup.py sdist bdist_wheel bdist_egg
+        cd ..
+    done
+    cd ..
+} || {
+    $PYTHON setup.py bdist_egg
+    cd pkgs
+    for pkg in *
+    do
+        cd $pkg
+        $PYTHON setup.py bdist_egg
+        cd ..
+    done
+    cd ..
+}
 
-echo "publishing build"
-cd dist
-tgz=$(ls -1rt burp-ui*.tar.gz | tail -1)
-egg=$(ls -1rt burp_ui*.egg | tail -1)
-whl=$(ls -1rt burp_ui*.whl | tail -1)
-cd ..
-cp -vf dist/burp-ui*.tar.gz /pub/ 2>/dev/null
-cp -vf dist/burp_ui*.egg /pub/ 2>/dev/null
-cp -vf dist/burp_ui*.whl /pub/ 2>/dev/null
-
-cd /pub
-#rm burp-ui.dev.tar.gz burp_ui-dev-py${VERSION}.egg burp_ui-dev-py2.py3-none-any.whl 2>/dev/null
-[ -n "$tgz" ] && ln -sf $tgz burp-ui.dev.tar.gz
-[ -n "$egg" ] && ln -sf $egg burp_ui-dev-py${VERSION}.egg
-[ -n "$whl" ] && ln -sf $whl burp_ui-dev-py2.py3-none-any.whl
+# Not useful anymore since we are using artifacts
+#echo "publishing build"
+#cd dist
+#tgz=$(ls -1rt burp-ui*.tar.gz | tail -1)
+#egg=$(ls -1rt burp_ui*.egg | tail -1)
+#whl=$(ls -1rt burp_ui*.whl | tail -1)
+#cd ..
+#cp -vf dist/burp-ui*.tar.gz /pub/ 2>/dev/null
+#cp -vf dist/burp_ui*.egg /pub/ 2>/dev/null
+#cp -vf dist/burp_ui*.whl /pub/ 2>/dev/null
+#
+#cd /pub
+##rm burp-ui.dev.tar.gz burp_ui-dev-py${VERSION}.egg burp_ui-dev-py2.py3-none-any.whl 2>/dev/null
+#[ -n "$tgz" ] && ln -sf $tgz burp-ui.dev.tar.gz
+#[ -n "$egg" ] && ln -sf $egg burp_ui-dev-py${VERSION}.egg
+#[ -n "$whl" ] && ln -sf $whl burp_ui-dev-py2.py3-none-any.whl
 
 exit 0
