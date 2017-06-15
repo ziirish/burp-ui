d46f68c3bb12d542bc6c0135f63dc735270661ff
ziirish <ziirish@ziirish.info>
2017-06-15 23:02:45 +0200

    new features for BASIC ACL backend

--- burpui/misc/acl/basic.py
+++ burpui/misc/acl/basic.py
@@ -1,12 +1,16 @@
 # -*- coding: utf8 -*-
 from .interface import BUIacl, BUIaclLoader
+from ...utils import make_list
 
+import re
 import json
+import fnmatch
 
 
 class ACLloader(BUIaclLoader):
     """See :class:`burpui.misc.acl.interface.BUIaclLoader`"""
     section = name = 'BASIC:ACL'
+    priority = 100
 
     def __init__(self, app=None):
         """See :func:`burpui.misc.acl.interface.BUIaclLoader.__init__`
@@ -18,9 +22,17 @@ class ACLloader(BUIaclLoader):
         self.admins = [
             'admin'
         ]
+        self.moderators = []
+        self.grants = {}
         self.clients = {}
         self.servers = {}
+        self.read_only = {}
+        self.read_write = {}
+        self.raw_servers = {}
         self.standalone = self.app.standalone
+        self.extended = False
+        self.legacy = False
+        self.moderator = {}
         self._acl = None
         self.conf_id = None
         self.conf = self.app.conf
@@ -32,28 +44,67 @@ class ACLloader(BUIaclLoader):
                 return False
 
         adms = []
+        mods = []
+
+        def is_empty(data):
+            return not data or data == [None]
+
         if self.section in self.conf.options:
-            adms = self.conf.safe_get('admin', 'force_list', section=self.section)
+            adms = self.conf.safe_get(
+                'admin',
+                'force_list',
+                section=self.section
+            )
+            mods = self.conf.safe_get(
+                'moderators',
+                'force_list',
+                section=self.section
+            )
+            self.priority = self.conf.safe_get(
+                'priority',
+                'integer',
+                section=self.section,
+                defaults={self.section: {'priority': self.priority}}
+            )
+            self.extended = self.conf.safe_get(
+                'extended',
+                'boolean',
+                section=self.section
+            )
+            self.legacy = self.conf.safe_get(
+                'legacy',
+                'boolean',
+                section=self.section
+            )
+            self.moderator = self.conf.safe_get(
+                'moderator',
+                'force_string',
+                section=self.section
+            ) or {}
             for opt in self.conf.options.get(self.section).keys():
-                if opt == 'admin':
+                if opt in ['admin', 'moderators', 'extended', 'priority', 'moderator', 'legacy']:
                     continue
-                lit = self.conf.safe_get(opt, section=self.section)
-                rec = []
-                try:
-                    rec = json.loads(lit)
-                    if isinstance(rec, dict):
-                        self.servers[opt] = rec.keys()
-                except Exception as e:
-                    self.logger.error(str(e))
-                    rec = [lit]
-                self.clients[opt] = rec
+                record = self.conf.safe_get(
+                    opt,
+                    'force_string',
+                    section=self.section
+                )
+                self.logger.debug('record: {} -> {}'.format(opt, record))
+                self.grants[opt] = record
 
-        if adms and adms != [None]:
+        if not is_empty(adms):
             self.admins = adms
+        if not is_empty(mods):
+            self.moderators = mods
 
-        self.logger.debug('admins: ' + str(self.admins))
-        self.logger.debug('clients: ' + str(self.clients))
-        self.logger.debug('servers: ' + str(self.servers))
+        if self.legacy:
+            self.extended = False
+
+        self.logger.debug('admins: {}'.format(self.admins))
+        self.logger.debug('moderators: {}'.format(self.moderators))
+        self.logger.debug('moderator grants: {}'.format(self.moderator))
+        self.logger.debug('extended: {}'.format(self.extended))
+        self.logger.debug('legacy: {}'.format(self.legacy))
 
         self.conf_id = self.conf.id
         self._acl = BasicACL(self)
@@ -71,20 +122,27 @@ class ACLloader(BUIaclLoader):
 
 class BasicACL(BUIacl):
     """See :class:`burpui.misc.acl.interface.BUIacl`"""
-    def __init__(self, handler=None):
+    def __init__(self, loader=None):
         """:func:`burpui.misc.acl.interface.BUIacl.__init__` instanciate ACL
         engine.
 
-        :param handler: ACL handler
-        :type handler: :class:`burpui.misc.acl.interface.BUIaclLoader`
+        :param loader: ACL loader
+        :type loader: :class:`burpui.misc.acl.interface.BUIaclLoader`
         """
-        if not handler:  # pragma: no cover
+        if not loader:  # pragma: no cover
             return
-        self.handler = handler
-        self.standalone = handler.standalone
-        self.admins = handler.admins
-        self.cls = handler.clients
-        self.srv = handler.servers
+        self.loader = loader
+        self.standalone = loader.standalone
+        self.admins = loader.admins
+        self.moderators = loader.moderators
+        self.moderator = loader.moderator
+        self.grants = loader.grants
+        self.extended = loader.extended
+        self.legacy = loader.legacy
+        self._parsed_grants = []
+        self._clients_cache = {}
+        self._agents_cache = {}
+        self._advanced_cache = {}
 
     def is_admin(self, username=None):
         """See :func:`burpui.misc.acl.interface.BUIacl.is_admin`"""
@@ -92,34 +150,137 @@ class BasicACL(BUIacl):
             return False
         return username in self.admins
 
-    def clients(self, username=None, server=None):
-        """See :func:`burpui.misc.acl.interface.BUIacl.clients`"""
-        if not username:  # pragma: no cover
-            return []
-        if username in self.cls:
-            cls = self.cls[username]
-            if server and isinstance(cls, dict):
-                if server in cls:
-                    return cls[server]
-                return []
-            # No server defined whereas we have an extended ACL
-            if not server and self.servers(username):
-                return []
-            return cls
-        return [username]
+    def is_moderator(self, username=None):
+        """See :func:`burpui.misc.acl.interface.BUIacl.is_moderator`"""
+        if not username:
+            return False
+        return username in self.moderators
 
-    def servers(self, username=None):
-        """See :func:`burpui.misc.acl.interface.BUIacl.servers`"""
-        if username and username in self.srv:
-            return self.srv[username]
-        return []
+    def _extract_grants(self, username):
+        if username not in self._parsed_grants:
+
+            if username == 'moderator':
+                grants = self.moderator
+            else:
+                grants = self.grants.get(username, '')
+            try:
+                grants = json.loads(grants)
+            except:
+                if ',' in grants:
+                    grants = grants.split(',')
+                else:
+                    grants = make_list(grants)
+
+            clients, agents, advanced = self._parse_clients(grants)
+            self._clients_cache[username] = clients
+            self._agents_cache[username] = agents
+            self._advanced_cache[username] = advanced
+
+            if self.is_moderator(username):
+                if 'moderator' not in self._parsed_grants:
+                    self._extract_grants('moderator')
+                self._clients_cache[username] = self._merge_data(
+                    self._clients_cache[username],
+                    self._clients_cache.get('moderator', [])
+                )
+                self._agents_cache[username] = self._merge_data(
+                    self._agents_cache[username],
+                    self._agents_cache.get('moderator', [])
+                )
+                self._advanced_cache[username] = self._merge_data(
+                    self._advanced_cache[username],
+                    self._advanced_cache.get('moderator', [])
+                )
+
+            self._parsed_grants.append(username)
+
+    def _extract_clients(self, username):
+        if username not in self._parsed_grants:
+            self._extract_grants(username)
+        return self._clients_cache.get(username, [])
+
+    def _extract_agents(self, username):
+        if username not in self._parsed_grants:
+            self._extract_grants(username)
+        return self._agents_cache.get(username, [])
+
+    def _extract_advanced(self, username):
+        if username not in self._parsed_grants:
+            self._extract_grants(username)
+        return self._advanced_cache.get(username, {})
+
+    def _extract_advanced_mode(self, username, mode, kind):
+        if username not in self._parsed_grants:
+            self._extract_grants(username)
+        return self._advanced_cache.get(username, {}).get(mode, {}).get(kind, [])
+
+    def _client_match(self, username, client):
+        clients = self._extract_clients(username)
+        if not clients:
+            return None
+
+        if self.extended:
+            for exp in clients:
+                regex = fnmatch.translate(exp)
+                if re.match(regex, client):
+                    return exp
+            return False
+        else:
+            return client if client in clients else False
+
+    def _server_match(self, username, server):
+        servers = self._extract_agents(username)
+        if not servers:
+            return None
+
+        if self.extended:
+            for exp in servers:
+                regex = fnmatch.translate(exp)
+                if re.match(regex, server):
+                    return exp
+            return False
+        else:
+            return server if server in servers else False
 
     def is_client_allowed(self, username=None, client=None, server=None):
         """See :func:`burpui.misc.acl.interface.BUIacl.is_client_allowed`"""
         if not username or not client:  # pragma: no cover
             return False
-        # No server defined whereas we have an extended ACL
-        if not server and self.servers(username):
+
+        is_admin = self.is_admin(username)
+        client_match = self._client_match(username, client)
+
+        if not client_match and username == client:
+            client_match = username
+
+        if server:
+            server_match = self._server_match(username, server)
+            if server_match is not None or self.legacy:
+                if not server_match:
+                    return is_admin
+
+                advanced = self._extract_advanced(username)
+
+                if not client_match and server_match not in advanced and \
+                        (server_match in self._extract_advanced_mode(username, 'ro', 'agents') or
+                         server_match in self._extract_advanced_mode(username, 'rw', 'agents')):
+                    return True
+
+                advanced = advanced.get(server_match, advanced.get(server, []))
+                if client_match not in advanced and client not in advanced:
+                    return is_admin
+
+        return client_match is not False or is_admin
+
+    def is_server_allowed(self, username=None, server=None):
+        """See :func:`burpui.misc.acl.interface.BUIacl.is_server_allowed`"""
+        if not username or not server:
             return False
-        cls = self.clients(username, server)
-        return (cls and client in cls) or self.is_admin(username)
+
+        server_match = self._server_match(username, server)
+        is_admin = self.is_admin(username)
+
+        if server_match is None and self.legacy:
+            server_match = False
+
+        return server_match is not False or is_admin
--- burpui/misc/acl/interface.py
+++ burpui/misc/acl/interface.py
@@ -7,8 +7,10 @@
 .. moduleauthor:: Ziirish <hi+burpui@ziirish.me>
 
 """
+from ...utils import make_list
+
 from abc import ABCMeta, abstractmethod, abstractproperty
-from six import with_metaclass
+from six import with_metaclass, iteritems
 
 import logging
 
@@ -50,10 +52,23 @@ class BUIacl(with_metaclass(ABCMeta, object)):
         :type username: str
 
         :returns: True if the user has admin rights, otherwise False
+        :rtype: bool
         """
         return False  # pragma: no cover
 
     @abstractmethod
+    def is_moderator(self, username=None):
+        """:func:`burpui.misc.acl.interface.BUIacl.is_moderator` is used to
+        know if a user has moderator rights.
+
+        :param username: Username to check
+        :type username: str
+
+        :returns: True if the user has moderator rights, otherwise False
+        :rtype: bool
+        """
+        return False  # pragma: no cover
+
     def clients(self, username=None, server=None):
         """:func:`burpui.misc.acl.interface.BUIacl.clients` returns a list of
         allowed clients for a given user.
@@ -65,10 +80,12 @@ class BUIacl(with_metaclass(ABCMeta, object)):
         :type server: str
 
         :returns: A list of clients
+        :rtype: list
+
+        ..deprecated:: 0.6.0
         """
         return []  # pragma: no cover
 
-    @abstractmethod
     def servers(self, username=None):
         """:func:`burpui.misc.acl.interface.BUIacl.servers` returns a list of
         allowed servers for a given user.
@@ -77,6 +94,9 @@ class BUIacl(with_metaclass(ABCMeta, object)):
         :type username: str
 
         :returns: A list of servers
+        :rtype: list
+
+        ..deprecated:: 0.6.0
         """
         return []  # pragma: no cover
 
@@ -95,5 +115,133 @@ class BUIacl(with_metaclass(ABCMeta, object)):
         :type server: str
 
         :returns: True if username is granted, otherwise False
+        :rtype: bool
         """
         return False  # pragma: no cover
+
+    def is_server_allowed(self, username=None, server=None):
+        """:func:`burpui.misc.acl.interface.BUIacl.is_server_allowed` tells us
+        if a given user has access to a given server.
+
+        :param username: Username to check
+        :type username: str
+
+        :param server: Server to check
+        :type server: str
+
+        :returns: True if username is granted, otherwise False
+        :rtype: bool
+        """
+        return False  # pragma: no cover
+
+    def _merge_data(self, d1, d2):
+        """Merge data as list or dict recursively avoiding duplicates"""
+        if not d2:
+            return d1
+        if not d1:
+            return d2
+        if isinstance(d1, list) and isinstance(d2, list):
+            return list(set(d1 + d2))
+        if isinstance(d1, list) and not isinstance(d2, dict):
+            if d2 in d1:
+                return d1
+            return d1 + [d2]
+        if isinstance(d2, list) and not isinstance(d1, dict):
+            if d1 in d2:
+                return d2
+            return d2 + [d1]
+        if not isinstance(d1, dict) and not isinstance(d2, dict):
+            if d1 == d2:
+                return [d1]
+            else:
+                return [d1, d2]
+
+        res = d1
+        for key2, val2 in iteritems(d2):
+            if key2 in res:
+                res[key2] = self._merge_data(val2, res[key2])
+            else:
+                res[key2] = val2
+        return res
+
+    def _parse_clients(self, data, mode=None):
+        agents = clients = []
+        advanced = {}
+        if isinstance(data, list):
+            if mode:
+                advanced[mode] = {'clients': data}
+            return data, agents, advanced
+        if not isinstance(data, dict):
+            if mode:
+                advanced[mode] = {'clients': make_list(data)}
+            return make_list(data), agents, advanced
+        for key, val in iteritems(data):
+            if key in ['agents', 'clients', 'ro', 'rw']:
+                continue
+            cl1, ag1, ad1 = self._parse_clients(val)
+            agents = self._merge_data(agents, ag1)
+            clients = self._merge_data(clients, cl1)
+            agents = self._merge_data(agents, key)
+            advanced = self._merge_data(advanced, ad1)
+            advanced = self._merge_data(advanced, {key: cl1})
+            if mode:
+                advanced = self._merge_data(advanced, {mode: {key: cl1}})
+
+        for key in ['clients', 'ro', 'rw']:
+            md = None
+            if key in data:
+                if key in ['ro', 'rw']:
+                    md = key
+                cl2, ag2, ad2 = self._parse_clients(data[key], md)
+                agents = self._merge_data(agents, ag2)
+                clients = self._merge_data(clients, cl2)
+                advanced = self._merge_data(advanced, ad2)
+
+        if 'agents' in data:
+            ag3, cl3, ad3 = self._parse_agents(data['agents'])
+            agents = self._merge_data(agents, ag3)
+            clients = self._merge_data(clients, cl3)
+            advanced = self._merge_data(advanced, ad3)
+
+        return make_list(clients), make_list(agents), advanced
+
+    def _parse_agents(self, data, mode=None):
+        agents = clients = []
+        advanced = {}
+        if isinstance(data, list):
+            if mode:
+                advanced[mode] = {'agents': data}
+            return data, clients, advanced
+        if not isinstance(data, dict):
+            if mode:
+                advanced[mode] = {'agents': make_list(data)}
+            return make_list(data), clients, advanced
+        for key, val in iteritems(data):
+            if key in ['agents', 'clients', 'ro', 'rw']:
+                continue
+            cl1, ag1, ad1 = self._parse_clients(data)
+            agents = self._merge_data(agents, ag1)
+            clients = self._merge_data(clients, cl1)
+            agents = self._merge_data(agents, key)
+            advanced = self._merge_data(advanced, ad1)
+            advanced = self._merge_data(advanced, {key: cl1})
+            if mode:
+                advanced = self._merge_data(advanced, {mode: {key: cl1}})
+
+        for key in ['agents', 'ro', 'rw']:
+            md = None
+            if key in data:
+                if key in ['ro', 'rw']:
+                    md = key
+                ag2, cl2, ad2 = self._parse_agents(data[key], md)
+                agents = self._merge_data(agents, ag2)
+                clients = self._merge_data(clients, cl2)
+                advanced = self._merge_data(advanced, ad2)
+
+        if 'clients' in data:
+            cl3, ag3, ad3 = self._parse_clients(data['clients'])
+            agents = self._merge_data(agents, ag3)
+            clients = self._merge_data(clients, cl3)
+            advanced = self._merge_data(advanced, ad3)
+
+        return make_list(agents), make_list(clients), advanced
