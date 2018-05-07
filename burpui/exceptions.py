601dab00a5e3022c376c9edc1d70d636294c2076
ziirish <ziirish@ziirish.info>
2018-05-07 19:12:56 +0200

    properly implement werkzeug exceptions

--- burpui/exceptions.py
+++ burpui/exceptions.py
@@ -10,16 +10,22 @@
 # Agent do not need "real" HTTP errors
 try:
     from werkzeug.exceptions import HTTPException
+    WERKZEUG = True
 except ImportError:
     HTTPException = Exception
+    WERKZEUG = False
 
 
 class BUIserverException(HTTPException):
     """Raised in case of internal error."""
     code = 500
 
-    def __init__(self, message):
-        self.description = message
+    def __init__(self, message="Internal Error", response=None):
+        if WERKZEUG:
+            HTTPException.__init__(self, message, response)
+        else:
+            self.description = message
+            self.response = response
 
     def __str__(self):
         return self.description
