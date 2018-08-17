059eb1a1b40ebcd205f7b1a45568e243b58a238b
ziirish <ziirish@ziirish.info>
2018-08-17 16:33:32 +0200

    python 3.7 compatibility

---
+++ burpui/ext/tasks.py
@@ -0,0 +1,10 @@
+# -*- coding: utf8 -*-
+"""
+.. module:: burpui.ext.tasks
+    :platform: Unix
+    :synopsis: Burp-UI external Tasks module.
+
+.. moduleauthor:: Ziirish <hi+burpui@ziirish.me>
+
+"""
+from celery import current_app as celery
